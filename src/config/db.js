022ed6c1a965ad20d35fc69791e9edd89288e27c
matias-lr <matias.larrondo.dev@gmail.com>
2020-10-03 18:26:59 -0300

    dale pipe con la pera sin hacer carreras

---
+++ src/config/db.js
@@ -0,0 +1,77 @@
+const oracledb = require("oracledb")
+
+oracledb.autoCommit = true;
+
+module.exports.select_procedure = async (nombre) =>{
+    let conect;
+    let string;
+    try{
+      conect = await oracledb.getConnection({
+        user:process.env.DB_USR,
+        password: process.env.DB_PW,
+        connectString: process.env.DBC
+      })
+      await conect.execute(
+        `BEGIN
+           DBMS_OUTPUT.ENABLE(NULL);
+         END;`);
+      await conect.execute(`BEGIN
+        ${nombre}_select();
+      END;`)
+      let check;
+      do {
+        check = await conect.execute(
+          `BEGIN
+             DBMS_OUTPUT.GET_LINE(:ln, :st);
+           END;`,
+          { ln: { dir: oracledb.BIND_OUT, type: oracledb.STRING, maxSize: 32767 },
+            st: { dir: oracledb.BIND_OUT, type: oracledb.NUMBER } });
+        if (check.outBinds.st === 0)
+          string = string + check.outBinds.ln
+      } while (check.outBinds.st === 0);
+      string = JSON.parse(string.replace('undefined','').replace('null','').replace(',]',']'))
+      return string;
+    }catch(err){
+      console.log(err)
+    }finally{
+      await conect.close()
+    }
+  }
+
+module.exports.insert_procedure = async (object) =>{
+    let conect;
+    try{
+        conect = await oracledb.getConnection({
+          user:process.env.DB_USR,
+          password: process.env.DB_PW,
+          connectString: process.env.DBC
+        })
+    const insert = object.insert.map(val => isNaN(val)?`''${val}''`:val).join(',')
+    console.log(insert)
+    await conect.execute(`
+        begin
+            insert_global('${object.tabla}','${insert}');
+        end;
+    `).then(response => console.log('funciono'))
+    .catch(err => console.log(error))
+    }catch(err){
+        console.log(err)
+    }finally{
+        conect.close()
+    }
+}
+module.exports.update_procedure = async (object) => {
+    let conect;
+    try{
+        conect = await oracledb.getConnection({
+          user:process.env.DB_USR,
+          password: process.env.DB_PW,
+          connectString: process.env.DBC
+        })
+    }
+    catch(err){
+        console.log(err)
+    }finally{
+        conect.close()
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const server = express()
+const db = require('./config/db')
 const cors = require('cors')
+const oracledb = require("oracledb")
 require('dotenv').config()
 
 const config = require('./config/cors')
@@ -18,3 +20,14 @@ server.get('/', function(req, res) {
 
 server.listen(port);
 console.log('application listen on port '+ port)
+/*db.select_procedure('usuario').then(val => console.log(val))
+var obj = {
+  tabla:'usuario',
+  insert:['matias','matias','email@email.com','asdhjsadhaksjd','201814871','alfredo acevedo 1044','ashdjkashdkasd',1]
+}
+db.insert_procedure(obj)*/
+var obj = {
+  tabla:'usuario',
+  update:[{columna:'nombre_columna',value:'valor a updatear'},{columna:'nombre_columna',value:'valor a updatear'}]
+}
+db.update_procedure(obj)
