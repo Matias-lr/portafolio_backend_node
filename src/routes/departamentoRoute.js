ea7208ea2e6105c7e6b242748bfe705e5f6b3c86
matias-lr <matias.larrondo.dev@gmail.com>
2020-11-15 21:19:47 -0300

    agregando el delete

--- src/routes/departamentoRoute.js
+++ src/routes/departamentoRoute.js
@@ -5,6 +5,6 @@ module.exports = (route) => {
     route.get('/departamentos',DepartamentoSelect)
     route.post('/departamento/create',[middleware.Authenticated,middleware.isAdmin],DepartamentoCreate)
     route.put('/departamento/:id/update',[middleware.Authenticated,middleware.isAdmin],DepartamentoUpdate)
-    route.delete('/departamento/:id/delete',[middleware.Authenticated,middleware.isAdmin],DepartamentoDelete)
     route.get('/departamentoByEdificio/:id',DepartamentoByEdificio)
+    route.delete('/departamento/:id/delete',[middleware.Authenticated,middleware.isAdmin],DepartamentoDelete)
 }
